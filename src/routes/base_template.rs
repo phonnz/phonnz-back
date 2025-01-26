a98e0ce1498ce365ce0998b6b129c877bd36de34
phonnz <gonzalezm.alfonso@gmail.com>
2025-01-26 20:38:28 +0100

    clean former files

--- src/routes/base_template.rs
+++ src/routes/base_template.rs
@@ -7,15 +7,28 @@ use super::{Route};
 pub struct Props {
     pub children: Children,
 }
-
+/**
+                *  <nav>
+                *   <Link<Route> to={Route::Home}>{ "Home" }</Link<Route>>
+                *</nav>
+                **/
 #[function_component(BaseTemplate)]
 pub fn base_template(props: &Props) -> Html {
     html! {
         <div class="base-template">
             <header>
-                <nav>
-                    <Link<Route> to={Route::Home}>{ "Home" }</Link<Route>>
-                </nav>
+               <img src="assets/images/phonnz.png" class="logo" alt="phonnz" />
+        <input class="side-menu" type="checkbox" id="side-menu"/>
+        <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
+        <nav class="nav">
+            <ul class="menu">
+              <li><a href="#home">{"Engineer"}</a></li>
+              <li><a href="#talks">{"Talks"}</a>
+              </li> <li><a href="#youtube">{"Youtube"}</a></li> 
+            <li><a href="#person">{"Person"}</a> </li>
+              <li><a href="#goals">{"Goals"}</a></li>
+            </ul>
+        </nav>
             </header>
             <main>
                 { for props.children.iter() }
