7530b32b72b919fb31d0793f4edfc126689c692e
phonnz <gonzalezm.alfonso@gmail.com>
2025-03-19 02:29:08 +0100

    Update Youtube template

--- src/routes/youtube/youtube_content.rs
+++ src/routes/youtube/youtube_content.rs
@@ -1,7 +1,7 @@
-
-use yew::{Component, Context, Html};
+use yew::{Component, Context, Html , html};
 
 const HTML: &str = include_str!("youtube.html");
+const CSS: &str = include_str!("youtube.css");
 
 pub struct YoutubeContent;
 
@@ -14,6 +14,15 @@ impl Component for YoutubeContent{
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
-        Html::from_html_unchecked(HTML.into())
+        let style_html = Html::from_html_unchecked(format!("<style>{}</style>", CSS).into());
+        
+        let content_html = Html::from_html_unchecked(HTML.into());
+        
+        html! {
+            <>
+                {style_html}
+                {content_html}
+            </>
+        }
     }
 }
